afa1583b26c579b0c879ac12ab7a7dc9477c8899
Asil <asilbekrakhimov4333@gmail.com>
2024-05-04 11:43:44 +0500

    Make delete and edit parts of Books section

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -1,6 +1,86 @@
+import fs from "fs";
+import path from "path";
+const __dirname = import.meta.dirname;
+
+const myPath = path.join(__dirname, "..", "db", "books.json");
 export function getAllBooks(req, res) {
-  console.log('Ishlayapti');
+  console.log("Ishlayapti");
   res.status(200).json({
     status: "OK",
   });
+}
+
+export function editBook(req, res) {
+  const bookID = +req.params.id;
+  const { image_link, title, author, price, year, description } = req.body;
+  if (!image_link || !title || !author || !price || !year || !description) {
+    res.status(400).json({
+      status: "Malumot toliq emas",
+    });
+  } else {
+    fs.readFile(myPath, "utf-8", (err, data) => {
+      if (err) {
+        throw err;
+      } else {
+        data = JSON.parse(data);
+        const oldBook = data.find((item) => item.id == bookID);
+        if (!oldBook) {
+          res.status(404).json({
+            status: "Malumot topilmadi",
+          });
+        } else {
+          data.forEach((element) => {
+            if (element.id == bookID) {
+              element.image_link = image_link;
+              element.title = title;
+              element.author = author;
+              element.price = price;
+              element.year = year;
+              element.description = description;
+            }
+          });
+          data = JSON.stringify(data);
+          fs.writeFileSync(myPath, data);
+          res.status(200).json({
+            status: "Updated",
+          });
+        }
+      }
+    });
+  }
+}
+
+export function deleteBook(req, res) {
+  try {
+    const bookID = req.params.id;
+    fs.readFile(myPath, "utf-8", (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+      else {
+        data = JSON.parse(data);
+        console.log(data);
+        let userIndex = -1;
+        data.forEach((item, index) => {
+          if (userID == item.id) {
+            userIndex = index;
+          }
+        });
+        if (userIndex == -1) {
+          res.status(404).json({
+            status: "Malumot topilmadi",
+          });
+        } else {
+          data.splice(userIndex, 1);
+          data = JSON.stringify(data);
+          fs.writeFileSync(myPath, data);
+          res.status(200).json({
+            status: "Deleted",
+          });
+        }
+      }
+    });
+  } catch (error) {
+    throw error;
+  }
 }
--- routes/books.routes.js
+++ routes/books.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { getAllBooks } from "../controllers/book.controller.js";
+import { editBook } from "../controllers/book.controller.js";
+import { deleteBook } from "../controllers/book.controller.js";
 
 const router = Router();
 
 router.get("/", getAllBooks);
+router.put('/:id',editBook);
+router.delete('/:id',deleteBook)
 // add commit
 
 export default router;
