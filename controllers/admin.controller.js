d079691a2db5ac77b0d3982b35063a361c87f72c
oybekjon08 <uzbbnbf@gmail.com>
2024-05-05 11:41:51 +0500

    updated

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,5 +1,35 @@
 import fs from "node:fs"
+import Joi from "joi"
 
+
+function validation(admin) {
+    const adminSchema = Joi.object({
+        fullname: Joi.string()
+            .alphanum()
+            .min(3)
+            .max(30)
+            .required(),
+        password: Joi.string()
+            .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
+            .min(4)
+            .max(16),
+        username: Joi.string()
+            .min(5)
+            .max(15)
+            .alphanum()
+            .required(),
+        age: Joi.number()
+            .integer()
+            .min(10),
+        gmail: Joi.string()
+            .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+        birth_year: Joi.number()
+            .integer()
+            .min(2000)
+            .max(2021)
+    })
+    return adminSchema.validate(admin)
+}
 
 export function getAllAdmins(req, res) {
     try {
@@ -59,97 +89,93 @@ export function getOneAdmin(req, res) {
 
 
 export function createAdmin(req, res) {
-    try{
-        let { fullname, age, gmail } = req.body
-        if (!fullname || !age || !gmail) {
-            res.status(403).send({
-                status: "Chala malumotlar"
-            })
+    try {
+        let result = validation(req.body)
+
+        if (result.error) {
+            res.status(400).send(result.error.details[0].message)
         } else {
             fs.readFile('./db/admin.json', 'utf8', (err, data) => {
                 if (err) { throw (err) }
                 data = JSON.parse(data)
                 let newAdmin = { "id": data.length + 1, ...req.body }
-                
+
                 data.push(newAdmin)
                 data = JSON.stringify(data)
                 fs.writeFileSync('./db.admin.json', data, err => {
                     if (err) { throw (err) }
                 })
                 data = JSON.parse(data)
-    
+
                 res.status(201).json({
-                    status:"Ok",
-                    data:data
+                    status: "Ok",
+                    data: data
                 })
             })
         }
-    } catch(err){
+    } catch (err) {
         console.log(err)
     }
 }
 
-export function editAdmin(req, res){
+export function editAdmin(req, res) {
     let id = req.params.id
-    let { fullname, age, gmail } = req.body
+    let result = validation(req.body)
 
-    fs.readFile('./db/admin.json', 'utf8', (err, data)=>{
-        if (!fullname || !age || !gmail) {
-            res.status(403).send({
-                status: "Chala malumotlar"
-            })
-        } else {
+    if (result.error) {
+        res.status(400).send(result.error.details[0].message)
+    } else {
+        fs.readFile('./db/admin.json', 'utf8', (err, data) => {
             data = JSON.parse(data)
-            data.forEach((element, index)=> {
-                if(id == element.id){
-                    data[index].fullname = fullname
-                    data[index].age = age
-                    data[index].gmail = gmail
+            data.forEach((element, index) => {
+                if (id == element.id) {
+                    data[index] = req.body
                 }
             });
             data = JSON.stringify(data)
-            fs.writeFileSync('./db/admin.json', data, err=>{
-                if(err){throw data}
+            fs.writeFileSync('./db/admin.json', data, err => {
+                if (err) { throw data }
                 else {
                     console.log("Malumotlar o'zgartildi")
                 }
             })
             data = JSON.parse(data)
             res.status(200).json({
-                status:"OK",
-                data : data
+                status: "OK",
+                data: data
             })
         }
-    })
+        )
+    }
 }
 
 
-export function deleteAdmin(req, res){
+export function deleteAdmin(req, res) {
     let id = req.params.id
 
-    fs.readFile('./db/admin.json', 'utf8', (err, data)=>{
-        if(err){throw err}
+    fs.readFile('./db/admin.json', 'utf8', (err, data) => {
+        if (err) { throw err }
 
         data = JSON.parse(data)
-        
-        data.forEach((item, index)=>{
-            if(item.id == id){
+
+        data.forEach((item, index) => {
+            if (item.id == id) {
                 data.splice(index, 1)
             }
         })
         data = JSON.stringify(data)
-        
-        fs.writeFileSync('./db/admin.json', data, err=>{
-            if(err){throw data}
+
+        fs.writeFileSync('./db/admin.json', data, err => {
+            if (err) { throw data }
             else {
                 console.log("Malumotlar o'zgartildi")
             }
         })
         data = JSON.parse(data)
         res.status(200).json({
-            status:"OK",
-            data : data
+            status: "OK",
+            data: data
         })
-        
+
     })
 }
